b069e2461fcefb060f3c708fb2d88432e4981511
feiyuji <ksjisl@126.com>
2019-07-04 15:43:31 +0800

    update

--- mongoModule/src/main/java/com/feiyu/controller/MongoController.java
+++ mongoModule/src/main/java/com/feiyu/controller/MongoController.java
@@ -2,16 +2,14 @@ package com.feiyu.controller;
 
 import com.feiyu.model.MongoTest;
 import com.feiyu.model.ResponseBean;
+import com.feiyu.model.StatusCode;
 import com.feiyu.service.MongoService;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import static org.slf4j.LoggerFactory.getLogger;
 
 @RestController
@@ -23,18 +21,19 @@ public class MongoController {
     private MongoService service;
 
     @RequestMapping("/")
-    public ResponseBean<MongoTest> findTestByName(@Param("name") String name) {
+    public ResponseEntity<ResponseBean<MongoTest>> findTestByName(@RequestParam("name") String name) {
         try {
             MongoTest testByName = service.findTestByName(name);
             if (testByName != null) {
-                return ResponseBean.success(testByName);
+                return ResponseEntity.ok(ResponseBean.success(testByName));
             } else {
-                return ResponseBean.successNoData();
+                logger.warn("fail to get mongo test name is {}", name);
+                return ResponseEntity.ok(ResponseBean.successNoData());
             }
         } catch (Exception e) {
             e.printStackTrace();
             logger.error("server is busy......  or  database issue...");
-            return ResponseBean.successNoData();
+            return ResponseEntity.ok(new ResponseBean(StatusCode.INTERNAL_ERROR.getCode(), "error"));
         }
     }
 }
