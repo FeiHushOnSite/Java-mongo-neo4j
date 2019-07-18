99bca2a89382f1087494dd85aa7aa0bc89778bc9
feiyuji <ksjisl@126.com>
2019-07-18 13:07:12 +0800

    update restTemplate

---
+++ neo4jModule/src/main/java/com/feiyu/config/RestConfig.java
@@ -0,0 +1,27 @@
+package com.feiyu.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.support.BasicAuthenticationInterceptor;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.Charset;
+
+@Configuration
+public class RestConfig {
+
+    @Value("message.username")
+    private String MassageUsername;
+    @Value("message.password")
+    private String MassagePassword;
+
+    @Bean
+    public RestTemplate messageTemplate(){
+        RestTemplate messageTemplate = new RestTemplate();
+        messageTemplate.getMessageConverters().add(0,new StringHttpMessageConverter(Charset.forName("utf-8")));
+        messageTemplate.getInterceptors().add(new BasicAuthenticationInterceptor(MassageUsername,MassagePassword));
+        return messageTemplate;
+    }
+}
